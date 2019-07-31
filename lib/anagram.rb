89d27681d7a7d9063bc390c34d85773bf51b9db8
David Little <littledavid175@gmail.com>
2019-07-31 18:33:30 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  
+  def initialize(string)
+    @word = string
+  end
+  
+  def match(string)
+   words = []
+   string.select do |element|
+     (@word.split("").sort) == (element.split("").sort)
+   end
+  end
+  
+end
